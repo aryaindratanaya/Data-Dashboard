e77f2697a73af066c9b8f832c77ccefb25be9b04
Indra Tanaya <aryaindratanaya@gmail.com>
2022-03-13 11:41:50 +0800

    Set the default selected key in the menu component

--- src/components/layout/TheMenu.tsx
+++ src/components/layout/TheMenu.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Menu } from 'antd'
 import {
   HomeOutlined,
@@ -7,15 +8,17 @@ import {
 } from '@ant-design/icons'
 
 export default function TheMenu() {
+  const router = useRouter()
+
   return (
-    <Menu theme="dark" mode="inline">
-      <Menu.Item key="home" icon={<HomeOutlined />}>
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={[router.pathname]}>
+      <Menu.Item key="/home" icon={<HomeOutlined />}>
         <Link href="/home">Home</Link>
       </Menu.Item>
-      <Menu.Item key="single-view" icon={<DatabaseOutlined />}>
+      <Menu.Item key="/single-view" icon={<DatabaseOutlined />}>
         <Link href="/single-view">Single View</Link>
       </Menu.Item>
-      <Menu.Item key="comparison-view" icon={<CompassOutlined />}>
+      <Menu.Item key="/comparison-view" icon={<CompassOutlined />}>
         <Link href="/comparison-view">Comparison View</Link>
       </Menu.Item>
     </Menu>
