31d2b8a1cc315ab4f73a6e567bbf1911aea1ba65
Indra Tanaya <aryaindratanaya@gmail.com>
2022-03-16 00:43:57 +0800

    Enable uploading csv data and populate it to a table

--- src/pages/single-view/index.tsx
+++ src/pages/single-view/index.tsx
@@ -1,21 +1,56 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Button } from 'antd'
-import s from 'styles/Home.module.css'
+import { Upload, Button, Table } from 'antd'
+
+const Papa = require('papaparse')
 
 const SingleView: NextPage = () => {
+  const [columns, setColumns] = useState([])
+  const [dataSource, setDataSource] = useState([])
+
+  const csvToJson = (file: any) => {
+    Papa.parse(file, {
+      header: true,
+      complete: (results: any) => {
+        const columns = results.meta.fields.map((item: string, i: number) => {
+          return {
+            key: i,
+            title: item,
+            dataIndex: item,
+          }
+        })
+
+        setColumns(columns)
+        setDataSource(results.data)
+      },
+    })
+  }
+
   return (
     <>
       <Head>
         <title>Data Dashboard - Single View</title>
       </Head>
 
-      <main>
-        <h1 className={s.mainHello}>Hi, this is the single-view page</h1>
-        <p>Below is an example of an Antd Button</p>
-        <Button type="primary">Button</Button>
-        <></>
-      </main>
+      <div>
+        <Upload
+          accept=".csv"
+          maxCount={1}
+          beforeUpload={(file) => {
+            csvToJson(file)
+            return false
+          }}
+        >
+          <Button>Select File</Button>
+        </Upload>
+
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          scroll={{ x: 'max-content' }}
+        />
+      </div>
     </>
   )
 }
