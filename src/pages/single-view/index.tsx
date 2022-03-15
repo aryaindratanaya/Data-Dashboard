2e7125fb3ffdba5c747a300a5dbf45a1f5404ff2
Indra Tanaya <aryaindratanaya@gmail.com>
2022-03-16 02:01:42 +0800

    Enable selecting some columns to show

--- src/pages/single-view/index.tsx
+++ src/pages/single-view/index.tsx
@@ -1,27 +1,32 @@
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Upload, Button, Table } from 'antd'
+import { Upload, Button, Table, Modal, Checkbox } from 'antd'
 
 const Papa = require('papaparse')
+const CheckboxGroup = Checkbox.Group
 
 const SingleView: NextPage = () => {
   const [columns, setColumns] = useState([])
+  const [sltColumns, setSltColumns] = useState([])
   const [dataSource, setDataSource] = useState([])
+  const [showModal, setShowModal] = useState(false)
 
   const csvToJson = (file: any) => {
     Papa.parse(file, {
       header: true,
       complete: (results: any) => {
-        const columns = results.meta.fields.map((item: string, i: number) => {
-          return {
-            key: i,
-            title: item,
-            dataIndex: item,
-          }
-        })
+        const columns = results.meta.fields.map(
+          (field: string, index: number) => ({
+            key: index,
+            title: field,
+            dataIndex: field,
+            displayed: true,
+          })
+        )
 
         setColumns(columns)
+        setSltColumns(columns.map(({ title }: { title: string }) => title))
         setDataSource(results.data)
       },
     })
@@ -44,6 +49,26 @@ const SingleView: NextPage = () => {
         >
           <Button>Select File</Button>
         </Upload>
+
+        <Button onClick={() => setShowModal(true)}>View</Button>
+        <Modal
+          title={`${sltColumns.length} column(s) are selected`}
+          visible={showModal}
+          onOk={() => {}}
+          onCancel={() => setShowModal(false)}
+        >
+          <CheckboxGroup
+            options={columns.map(({ title }) => title)}
+            // value={Array.from(
+            //   columns,
+            //   ({ title, displayed }) => displayed && title
+            // )}
+            value={sltColumns}
+            onChange={(sltTitles: any) => {
+              setSltColumns(sltTitles)
+            }}
+          />
+        </Modal>
 
         <Table
           columns={columns}
